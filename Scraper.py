8b688082e017b4ebc89417445a423dac1a0132c8
Sahrarara <74914665+Sahrarara@users.noreply.github.com>
2021-07-01 14:48:23 +0200

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import re
 
 
 print('Program has started')
@@ -25,7 +26,8 @@ WebDriverWait(driver,3)
 search_form = driver.find_element_by_xpath('/html/body/div[1]/div/div/form/div[3]/button').click()	#clicks the dropdown box and presses the search button
 submit_button = driver.find_element_by_id('neugruendungen_searchbutton')
 submit_button.click()
-print('Finally done with the preparation')
+print('Preparation done. Loading data sets')
+
 
 loadmorebutton=driver.find_element_by_class_name('show-more-news-link')					#clicks the "show more" button to load more companies
 i=10
@@ -36,35 +38,56 @@ while loadmorebutton.size!=0:										#this is a mess. It clicks the "show more
 	except:
 		break
 	finally:
-		print('loaded')
-		print(i)
+		print('loaded ' , i , 'more data sets')
 		i=i+10
-		print('more data sets')
 		print('')
 
-print('Starting to scrap!')											
+print('Starting to scrap!')
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'link-list__link')))	#we wait a bit and then start scraping
 print('')
+
+namelist=[]
+desclist=[]
+addrlist=[]
+linklist=[]
+i=1
 print('Results are being loaded. Please wait.')
-company_name=driver.find_elements_by_class_name('link-list__link')					#here we get all the data which you can find in the readme.md and prints it
+company_name=driver.find_elements_by_class_name('link-list__link')					#here we get most data. Only the address is hidden behind the href link
 company_description=driver.find_elements_by_class_name('link-list__desc')
 wko_link=driver.find_elements_by_class_name('link-list__link')
 for x in range(len(company_name)):
-	print(company_name[x].text)
-	print(company_description[x].text)
-	print("WKO Link: " + wko_link[x].get_attribute('href'))
-	print('')
+	namelist.append(company_name[x].text)
+	desclist.append(company_description[x].text)
+	linklist.append(wko_link[x].get_attribute('href'))
+	print('Appending to lists.', i , 'appended items')
+	i=i+1
+print('appended all' , i-1 , 'items. Starting next task')
+
+j=1
+try:
+	for x in range(len(linklist)):									#uses the links in the linklist to open the site and scrape the address from there.
+		if linklist[x]!='https://www.wko.at/service/neuzugaenge.html#':
+			driver.get(linklist[x])
+			WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.row:nth-child(2) > div:nth-child(1) > p:nth-child(2) > span:nth-child(2)')))
+			addr=driver.find_element_by_css_selector('div.row:nth-child(2) > div:nth-child(1) > p:nth-child(2) > span:nth-child(2)').text
+			addrlist.append(re.sub("\(kann vom Gründungsdatum abweichen\)", "", addr))
+			print('got address nr.' , j)							#printing the number so you can easier find it in the csv file
+			j=j+1
+		else:
+			addrlist.append('bad link, thus no data')					#sometimes there is a link to the wko site itself. that's considered a bad link
+			print('bad link')
+			j=j+1
+except:
+	print('an error has occured')
 
 print('Writing data in csv')										#a print command so you know that the program is working. it just takes a while
-
+j=1
 with open('newCompanies.csv', 'w', newline='') as file:							#the data is being written in a csv file.
-	company_name=driver.find_elements_by_class_name('link-list__link')
-	company_description=driver.find_elements_by_class_name('link-list__desc')
-	wko_link=driver.find_elements_by_class_name('link-list__link')
 	writer = csv.writer(file)
-	for x in range(len(company_name)):
-		writer.writerow([company_name[x].text])
-		writer.writerow([company_description[x].text])
-		writer.writerow(["WKO Link: ", wko_link[x].get_attribute('href')])
-		writer.writerow([])
+	writer.writerow(['', 'Firmenname', 'Adresse', 'Beschreibung', 'WKO-Link'])
+	for x in range(len(namelist)):
+		writer.writerow([j, namelist[x], addrlist[x], desclist[x], linklist[x]])
+		print(j, ' of ' ,i-1, ' items have been written in the csv')
+		j=j+1
 	driver.quit()
+print('Data has been succesfully scraped')
